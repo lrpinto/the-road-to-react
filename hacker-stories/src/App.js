9d6bcf632f8a8d1bcfcab8225bead9a16ebb1a6d
Luisa <luisa.pinto@footasylum.com>
2021-08-11 16:43:31 +0100

    Lists in React p17

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.js.org',
+    author: 'Jordan Walke',
+    nume_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    nume_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+]
 
 function App() {
   // do something in between
