c9f766afd123e18fa49af47cc3ccbfc4a061d89c
Luisa <luisa.pinto@footasylum.com>
2021-08-11 17:41:46 +0100

    React Props p36

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,26 +1,25 @@
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://react.js.org',
-    author: 'Jordan Walke',
-    nume_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-]
-
 const App = () => {
-  
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://react.js.org',
+      author: 'Jordan Walke',
+      nume_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    },
+  ]
+
   const handleChange = event => {
     console.log(event.target.value);
   }
