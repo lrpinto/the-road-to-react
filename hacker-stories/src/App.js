a9a26cd82d6dffd9e33d5ae328a15579db9a75de
Luisa <luisa.pinto@footasylum.com>
2021-08-11 18:13:46 +0100

    Lifting state in React p48

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -27,7 +27,7 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter( story => story.title.includes(searchTerm))
+  const searchedStories = stories.filter( story => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
   
   return (
     <div>
