0dfdfc3a3ddfbebc9be81e652159c8198b0cd4d6
Luisa <luisa.pinto@footasylum.com>
2021-08-11 17:03:17 +0100

    Lists in React p21

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -13,7 +13,7 @@ const list = [
     title: 'Redux',
     url: 'https://redux.js.org',
     author: 'Dan Abramov, Andrew Clark',
-    nume_comments: 2,
+    num_comments: 2,
     points: 5,
     objectID: 1
   },
@@ -32,7 +32,16 @@ function App() {
       <hr />
 
       {list.map(function(item){
-        return (<div key={item.objectID}>{item.title}</div>)
+        return (
+          <div key={item.objectID}>{item.title}
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
       })}
     </div>
   );
